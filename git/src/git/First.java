2a20227143ad98cf849f10125984f23c64385cfc
jiege <1608128951@qq.com>
2020-11-30 15:34:06 +0800

    把项目添加进git

--- git/src/git/First.java
+++ git/src/git/First.java
@@ -5,4 +5,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello World Git");
     }
+    public void test(){
+        int i = 10;
+        i += 10;
+    }
 }
